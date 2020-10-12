f0150a1757467dee4399e44b5654320c617a2f4d
Tala Agha <71174110+TalaAgha@users.noreply.github.com>
2020-10-12 23:27:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+ var close = document.querySelector(".close").addEventListener("click", toggle);
+ var play = document.querySelector(".play").addEventListener("click", toggle);
+
+function toggle() {
+ 
+ 
+ var trailer = document.querySelector(".trailer");
+var video = document.querySelector(".iframe");
+
+    trailer.classList.toggle(".active");
+
+
+ 
+}
